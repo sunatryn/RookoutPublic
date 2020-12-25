cacc78a82ebcab288f07282659522ec2db3438b7
Thundra Temp <thundratemp@Thundras-MacBook-Pro.local>
2020-12-25 11:59:20 +0300

    test commit

--- src/handlers/homePage.js
+++ src/handlers/homePage.js
@@ -1,3 +1,12 @@
+const rookout = require('rookout');
+
+rookout.start({
+    token: '83aecbbfd1e4fcfc1b8e053d422aaef4f9840b23a405ecb1ffdaa9bb535b2afc',
+    labels: {
+      env: 'dev'
+    }
+})
+
 const getTodosByFilter = require('../services/getTodosByFilter');
 
 const homePage = async (req, res) => {
